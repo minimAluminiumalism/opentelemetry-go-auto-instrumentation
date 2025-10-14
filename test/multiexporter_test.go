b25f48830a89c8a9fe535b21f28e7c13b045b42c
wilsonxscai <wilsonxscai@tencent.com>
2025-10-14 18:58:05 +0800

    fix license err in docs

--- test/multiexporter_test.go
+++ test/multiexporter_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Alibaba Group Holding Ltd.
+// Copyright (c) 2025 Alibaba Group Holding Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import (
 
 func TestMultiExporter(t *testing.T) {
 	UseApp("multiexporter")
-	RunGoBuild(t, "go", "build", "--", "test_multi_exporter.go")
+	RunGoBuild(t, "go", "build", "test_multi_exporter.go")
 	
 	env := []string{
 		"OTEL_TRACES_EXPORTER=console,otlp",
